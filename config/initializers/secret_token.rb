0cbd9070c5191422b83006b24cd19d10c5695b10
Harry Boileau <harrysboileau@gmail.com>
2014-05-18 11:37:06 -0500

    Initialize respository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PupularApiForIos::Application.config.secret_key_base = 'c69b4b1a21bf12239c42706223ca87defdc387ed90926de5910a77cc9dfa6603b709b0ee0b2f9f585ba46e02c169d46989e9bee742260608f1b5a2ed3f1c811a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PupularApiForIos::Application.config.session_store :cookie_store, key: '_pupular_api_for_ios_session'
